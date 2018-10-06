ac384d8f8b18a33f38a0c3fb52f254db50885537
Yaldaba0th <serketnet@gmail.com>
2018-10-06 06:28:10 -0300

    cambio entre dos layouts, llevar variables de un layout a otro

--- app/src/main/java/android/example/com/materiales/MainActivity.java
+++ app/src/main/java/android/example/com/materiales/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void openFile(View view) {
+        int intID = view.getId();
+        Button button = (Button) findViewById(intID);
+        String name = button.getText().toString();
+        setContentView(R.layout.activity_second);
+        TextView ficha = (TextView) findViewById(R.id.show_title);
+        ficha.setText(name);
+
+    }
+
+    public void regresar(View view) {
+        setContentView(R.layout.activity_main);
     }
 }
